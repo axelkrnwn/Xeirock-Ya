308a954f6468af48516a32fc504011cb7fd605d1
aLeX90904002 <axelkurn@gmail.com>
2024-06-30 00:18:39 +0700

    fix : revision register page

--- register.js
+++ register.js
@@ -1,9 +1,13 @@
-document.getElementById('first-form').addEventListener('submit', (e) => {
+document.getElementById('next-btn').addEventListener('click', (e) => {
     e.preventDefault()
-    document.getElementById('form-image').style.left = 0
+    document.getElementById('form-image').style.left = "0"
 })
 
-document.getElementById('second-form').addEventListener('submit', (e) => {
+document.getElementById('back-btn').addEventListener('click', (e) => {
     e.preventDefault()
-    window.location.href = '/'
+    document.getElementById('form-image').style.left = "50%"
+})
+document.getElementById('form-1').addEventListener('submit', (e) => {
+    e.preventDefault()
+    window.location.href = './index.html'
 })
