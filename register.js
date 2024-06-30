17b55c8f181a98074ad93198f5cea6a60fc2800a
aLeX90904002 <axelkurn@gmail.com>
2024-06-30 22:56:13 +0700

    fix : revision part 2

--- register.js
+++ register.js
@@ -1,11 +1,11 @@
 document.getElementById('next-btn').addEventListener('click', (e) => {
     e.preventDefault()
-    document.getElementById('form-image').style.left = "0"
+    document.getElementById('form-image').style.transform = "translateX(0)"
 })
 
 document.getElementById('back-btn').addEventListener('click', (e) => {
     e.preventDefault()
-    document.getElementById('form-image').style.left = "50%"
+    document.getElementById('form-image').style.transform = "translateX(100%)"
 })
 document.getElementById('form-1').addEventListener('submit', (e) => {
     e.preventDefault()
